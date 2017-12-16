d30d210a5c63789c7bcbc1d59eae9073e7e265c4
Vu Phan <arsenalvu@gmail.com>
2017-12-16 12:48:59 +0700

    merge with winform

--- TOURDULICH_WIN/TOURDULICH_WIN/BAL/TOURDIADIEM_BAL.cs
+++ TOURDULICH_WIN/TOURDULICH_WIN/BAL/TOURDIADIEM_BAL.cs
@@ -22,6 +22,15 @@ namespace TOURDULICH_WIN.BAL
             List<Tour_DiaDiem> tour_dd = tourdd_db.Search(x => x.MaTour == ma).ToList();
             return tour_dd;
         }
-
+        public bool Insert(Tour_DiaDiem t)
+        {
+            return tourdd_db.Insert(t);
+        }
+        public bool Delete(int ma)
+        {
+            Tour_DiaDiem tdd = tourdd_db.GetSingle(x => x.MaTDD == ma);
+            return tourdd_db.Delete(tdd);
+        }
+    
     }
 }
